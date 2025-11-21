5d95c628895766e2e02cadf77744fadf961f6b8e
Ashish <ashishtaduce2@gmail.com>
2025-11-21 17:24:45 +0530

    Added network support and success callback

--- lib/flutter_form_generator.dart
+++ lib/flutter_form_generator.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'utils/action_engine.dart';
 import 'utils/common_utils.dart';
 
 import 'models/field_info.dart';
@@ -7,65 +8,66 @@ import 'widgets/checkbox_form_field.dart';
 import 'widgets/password_form_field.dart';
 
 class FormGenerator {
-  final Map<String, dynamic> _values = {};
+  final Map<String, String>? headers;
+  final void Function(Map<String, dynamic> values)? onSuccess;
+
+  late final ActionEngine _actionEngine;
+  final Map<GlobalKey<FormState>, Map<String, dynamic>> _formValues = {};
+
+  FormGenerator({
+    this.headers,
+    this.onSuccess,
+  }) {
+    _actionEngine = ActionEngine(headers: headers);
+  }
 
   Widget generateForm(Map<String, dynamic> formData) {
     GlobalKey<FormState> formKey = GlobalKey<FormState>();
+    _formValues[formKey] = {};
 
-    return Form(
-      key: formKey,
-      child: SingleChildScrollView(
-        child: Column(
-          spacing: 16.0,
-          children: [
-            ..._generateFields(
-              (formData['fields'] as List)
-                  .map((field) => FormFieldInfo.fromJson(field))
-                  .toList(),
-            ),
-            SizedBox(height: 8.0),
-            Row(
-              spacing: 16.0,
-              children: [
-                if (formData['submit'] != null)
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: ElevatedButton(
-                      onPressed: () => _handleSubmit(formKey),
-                      child: Text(formData['submit']?['label'] ?? 'Submit'),
-                    ),
-                  ),
-                ),
-                if (formData['reset'] != null)
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: OutlinedButton(
-                      onPressed: () => _handleReset(formKey),
-                      child: Text('Reset'),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
+    return _FormGeneratorWidget(
+      formKey: formKey,
+      formData: formData,
+      formGenerator: this,
     );
   }
 
-  void _handleSubmit(GlobalKey<FormState> formKey) {
+  void _handleSubmit(
+      GlobalKey<FormState> formKey,
+      Map<String, dynamic> formData,
+      BuildContext context,
+      Function(bool) setLoading,
+      ) async {
     if (formKey.currentState?.validate() ?? false) {
       formKey.currentState?.save();
-      debugPrint(_values.toString());
+
+      final submitAction = formData['submit']?['action'];
+
+      if (submitAction != null) {
+        setLoading(true);
+
+        await _actionEngine.executeAction(
+          action: submitAction,
+          context: context,
+          formValues: _formValues[formKey],
+          onSuccess: () {
+            if (onSuccess != null) {
+              onSuccess!(_formValues[formKey]!);
+            }
+          },
+        );
+
+        setLoading(false);
+      } else {
+        print(_formValues[formKey]);
+        if (onSuccess != null) {
+          onSuccess!(_formValues[formKey]!);
+        }
+      }
     }
   }
 
-  void _handleReset(GlobalKey<FormState> formKey) {
-    final context = formKey.currentContext;
-    if (context == null) return;
-
+  void _handleReset(GlobalKey<FormState> formKey, BuildContext context) {
     showDialog(
       context: context,
       builder: (BuildContext dialogContext) {
@@ -80,7 +82,7 @@ class FormGenerator {
             TextButton(
               onPressed: () {
                 formKey.currentState?.reset();
-                _values.clear();
+                _formValues[formKey]?.clear();
                 Navigator.of(dialogContext).pop();
               },
               child: Text('Reset'),
@@ -91,15 +93,18 @@ class FormGenerator {
     );
   }
 
-  List<Widget> _generateFields(List<FormFieldInfo> fieldsList) {
+  List<Widget> _generateFields(
+      List<FormFieldInfo> fieldsList,
+      GlobalKey<FormState> formKey,
+      ) {
     List<Widget> fields = [];
     for (var field in fieldsList) {
-      fields.add(_generateField(field));
+      fields.add(_generateField(field, formKey));
     }
     return fields;
   }
 
-  Widget _generateField(FormFieldInfo fieldInfo) {
+  Widget _generateField(FormFieldInfo fieldInfo, GlobalKey<FormState> formKey) {
     late Widget field;
     String fieldLabel = fieldInfo.label;
 
@@ -110,7 +115,7 @@ class FormGenerator {
             labelText: fieldLabel,
             hintText: fieldLabel,
           ),
-          onSaved: (value) => _values[fieldInfo.name] = value,
+          onSaved: (value) => _formValues[formKey]![fieldInfo.name] = value,
           validator: (value) {
             if (fieldInfo.required && (value?.isEmpty ?? true)) {
               return "Please enter some text";
@@ -142,7 +147,7 @@ class FormGenerator {
             labelText: fieldLabel,
             hintText: fieldLabel,
           ),
-          onSaved: (value) => _values[fieldInfo.name] = value,
+          onSaved: (value) => _formValues[formKey]![fieldInfo.name] = value,
           validator: (value) {
             if (fieldInfo.required && (value?.isEmpty ?? true)) {
               return "Please enter some text";
@@ -169,7 +174,7 @@ class FormGenerator {
             labelText: fieldLabel,
             hintText: fieldLabel,
           ),
-          onSaved: (value) => _values[fieldInfo.name] = value,
+          onSaved: (value) => _formValues[formKey]![fieldInfo.name] = value,
           validator: (value) {
             if (fieldInfo.required && (value?.isEmpty ?? true)) {
               return "Please enter some text";
@@ -184,7 +189,7 @@ class FormGenerator {
       case FieldType.passwordInput:
         field = PasswordFormField(
           fieldInfo: fieldInfo,
-          onSaved: (value) => _values[fieldInfo.name] = value,
+          onSaved: (value) => _formValues[formKey]![fieldInfo.name] = value,
         );
 
       case FieldType.textArea:
@@ -193,7 +198,7 @@ class FormGenerator {
             labelText: fieldLabel,
             hintText: fieldLabel,
           ),
-          onSaved: (value) => _values[fieldInfo.name] = value,
+          onSaved: (value) => _formValues[formKey]![fieldInfo.name] = value,
           validator: (value) {
             if (fieldInfo.required && (value?.isEmpty ?? true)) {
               return "Please enter some text";
@@ -224,15 +229,13 @@ class FormGenerator {
             labelText: fieldLabel,
             hintText: fieldLabel,
           ),
-          onSaved: (value) => _values[fieldInfo.name] = value,
+          onSaved: (value) => _formValues[formKey]![fieldInfo.name] = value,
           onChanged: (value) {},
           items: (fieldInfo.options ?? [])
-              .map(
-                (optionInfo) => DropdownMenuItem(
-                  value: optionInfo.value,
-                  child: Text(optionInfo.label),
-                ),
-              )
+              .map((optionInfo) => DropdownMenuItem(
+            value: optionInfo.value,
+            child: Text(optionInfo.label),
+          ))
               .toList(),
           validator: (value) {
             if (fieldInfo.required && (value == null || value.isEmpty)) {
@@ -240,16 +243,14 @@ class FormGenerator {
             }
 
             return checkValidations(
-              fieldInfo.validations ?? [],
-              value?.toString() ?? '',
-            );
+                fieldInfo.validations ?? [], value?.toString() ?? '');
           },
         );
 
       case FieldType.checkbox:
         field = CheckboxFormField(
           fieldInfo: fieldInfo,
-          onSaved: (value) => _values[fieldInfo.name] = value,
+          onSaved: (value) => _formValues[formKey]![fieldInfo.name] = value,
         );
 
       default:
@@ -257,5 +258,96 @@ class FormGenerator {
     }
 
     return field;
+  }
+}
+
+class _FormGeneratorWidget extends StatefulWidget {
+  final GlobalKey<FormState> formKey;
+  final Map<String, dynamic> formData;
+  final FormGenerator formGenerator;
+
+  const _FormGeneratorWidget({
+    required this.formKey,
+    required this.formData,
+    required this.formGenerator,
+  });
+
+  @override
+  State<_FormGeneratorWidget> createState() => _FormGeneratorWidgetState();
+}
+
+class _FormGeneratorWidgetState extends State<_FormGeneratorWidget> {
+  bool _isLoading = false;
+
+  void _setLoading(bool loading) {
+    setState(() {
+      _isLoading = loading;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: widget.formKey,
+      child: SingleChildScrollView(
+        child: Column(
+          spacing: 16.0,
+          children: [
+            ...widget.formGenerator._generateFields(
+              (widget.formData['fields'] as List)
+                  .map((field) => FormFieldInfo.fromJson(field))
+                  .toList(),
+              widget.formKey,
+            ),
+            SizedBox(height: 8.0),
+            Row(
+              spacing: 16.0,
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: ElevatedButton(
+                      onPressed: _isLoading
+                          ? null
+                          : () => widget.formGenerator._handleSubmit(
+                        widget.formKey,
+                        widget.formData,
+                        context,
+                        _setLoading,
+                      ),
+                      child: _isLoading
+                          ? SizedBox(
+                        height: 20,
+                        width: 20,
+                        child: CircularProgressIndicator(
+                          strokeWidth: 2,
+                        ),
+                      )
+                          : Text(
+                        widget.formData['submit']?['label'] ?? 'Submit',
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: OutlinedButton(
+                      onPressed: _isLoading
+                          ? null
+                          : () => widget.formGenerator._handleReset(
+                        widget.formKey,
+                        context,
+                      ),
+                      child: Text('Reset'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/utils/action_engine.dart
@@ -0,0 +1,158 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class ActionEngine {
+  final Dio _dio;
+  final Map<String, String> headers;
+
+  ActionEngine({Map<String, String>? headers})
+    : headers = headers ?? {},
+      _dio = Dio(
+        BaseOptions(
+          headers: headers ?? {'Content-Type': 'application/json'},
+          validateStatus: (status) => status != null && status < 500,
+        ),
+      );
+
+  Future<void> executeAction({
+    required Map<String, dynamic> action,
+    required BuildContext context,
+    Map<String, dynamic>? formValues,
+    VoidCallback? onSuccess,
+  }) async {
+    final actionType = action['type'] as String?;
+
+    switch (actionType) {
+      case 'api_call':
+        await _handleApiCall(
+          action: action,
+          context: context,
+          formValues: formValues,
+          onSuccess: onSuccess,
+        );
+        break;
+
+      case 'open_link':
+        await _handleOpenLink(action: action, context: context);
+        break;
+
+      default:
+        _showMessage(
+          context: context,
+          message: 'Unknown action type: $actionType',
+          isError: true,
+        );
+    }
+  }
+
+  Future<void> _handleApiCall({
+    required Map<String, dynamic> action,
+    required BuildContext context,
+    Map<String, dynamic>? formValues,
+    VoidCallback? onSuccess,
+  }) async {
+    final method = (action['method'] as String?)?.toUpperCase() ?? 'POST';
+    final url = action['url'] as String?;
+    final successMessage = action['success_message'] as String? ?? 'Success';
+    final errorMessage =
+        action['error_message'] as String? ?? 'An error occurred';
+
+    if (url == null || url.isEmpty) {
+      _showMessage(context: context, message: 'Invalid URL', isError: true);
+      return;
+    }
+
+    try {
+      Response? response;
+
+      switch (method) {
+        case 'GET':
+          response = await _dio.get(url);
+          break;
+        case 'POST':
+          response = await _dio.post(url, data: formValues);
+          break;
+        case 'PUT':
+          response = await _dio.put(url, data: formValues);
+          break;
+        case 'DELETE':
+          response = await _dio.delete(url);
+          break;
+        case 'PATCH':
+          response = await _dio.patch(url, data: formValues);
+          break;
+        default:
+          _showMessage(
+            context: context,
+            message: 'Unsupported HTTP method: $method',
+            isError: true,
+          );
+          return;
+      }
+
+      if (response.statusCode != null &&
+          response.statusCode! >= 200 &&
+          response.statusCode! < 300) {
+        _showMessage(context: context, message: successMessage, isError: false);
+        onSuccess?.call();
+      } else {
+        _showMessage(context: context, message: errorMessage, isError: true);
+      }
+    } on DioException catch (e) {
+      print('API Error: ${e.message}');
+      _showMessage(context: context, message: errorMessage, isError: true);
+    } catch (e) {
+      print('Unexpected Error: $e');
+      _showMessage(context: context, message: errorMessage, isError: true);
+    }
+  }
+
+  Future<void> _handleOpenLink({
+    required Map<String, dynamic> action,
+    required BuildContext context,
+  }) async {
+    final url = action['url'] as String?;
+
+    if (url == null || url.isEmpty) {
+      _showMessage(context: context, message: 'Invalid URL', isError: true);
+      return;
+    }
+
+    try {
+      final uri = Uri.parse(url);
+      final canLaunch = await canLaunchUrl(uri);
+
+      if (canLaunch) {
+        await launchUrl(uri, mode: LaunchMode.inAppWebView);
+      } else {
+        _showMessage(
+          context: context,
+          message: 'Cannot open link',
+          isError: true,
+        );
+      }
+    } catch (e) {
+      print('Link Error: $e');
+      _showMessage(
+        context: context,
+        message: 'Failed to open link',
+        isError: true,
+      );
+    }
+  }
+
+  void _showMessage({
+    required BuildContext context,
+    required String message,
+    required bool isError,
+  }) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        backgroundColor: isError ? Colors.red : Colors.green,
+        duration: Duration(seconds: 3),
+      ),
+    );
+  }
+}
